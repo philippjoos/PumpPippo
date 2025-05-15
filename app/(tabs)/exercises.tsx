00bc0ed58f1ceb93c0eb900f990f7eae58d16731
Philipp Joos <joosphi1@students.zhaw.ch>
2025-05-15 13:13:17 +0200

    fixed initial exercises

--- app/(tabs)/exercises.tsx
+++ app/(tabs)/exercises.tsx
@@ -30,7 +30,8 @@ export default function exercises() {
 
   useEffect(() => {
     FileHandler.getExercises().then((loadedExercises) => {
-      if (loadedExercises) {
+      console.log(loadedExercises);
+      if (loadedExercises && loadedExercises.length > 0) {
         setExercises(loadedExercises as Exercise[]);
       } else {
         FileHandler.saveData('exercises', [
@@ -38,101 +39,61 @@ export default function exercises() {
             "name": "Barbell Bench Press",
             "muscle_group": "Chest",
             "equipment": "Barbell, Bench",
-            "sets": [
-              { "count": 1, "reps": 10, "weight": 60, "rest_time": 180 },
-              { "count": 2, "reps": 8, "weight": 70, "rest_time": 180 },
-              { "count": 3, "reps": 6, "weight": 80, "rest_time": 180 }
-            ]
+            "sets": []
           },
           {
             "name": "Squat",
             "muscle_group": "Legs",
             "equipment": "Barbell, Rack (optional)",
-            "sets": [
-              { "count": 1, "reps": 10, "weight": 80, "rest_time": 180 },
-              { "count": 2, "reps": 8, "weight": 100, "rest_time": 180 },
-              { "count": 3, "reps": 6, "weight": 120, "rest_time": 180 }
-            ]
+            "sets": []
           },
           {
             "name": "Deadlift",
             "muscle_group": "Back, Glutes, Hamstrings",
             "equipment": "Barbell",
-            "sets": [
-              { "count": 1, "reps": 10, "weight": 100, "rest_time": 180 },
-              { "count": 2, "reps": 8, "weight": 120, "rest_time": 180 },
-              { "count": 3, "reps": 6, "weight": 140, "rest_time": 180 }
-            ]
+            "sets": []
           },
           {
             "name": "Pull-Up",
             "muscle_group": "Back, Biceps",
             "equipment": "Pull-Up Bar",
-            "sets": [
-              { "count": 1, "reps": 8, "weight": 0, "rest_time": 120 },
-              { "count": 2, "reps": 6, "weight": 0, "rest_time": 120 },
-              { "count": 3, "reps": 5, "weight": 0, "rest_time": 120 }
-            ]
+            "sets": []
           },
           {
             "name": "Overhead Press",
             "muscle_group": "Shoulders",
             "equipment": "Barbell",
-            "sets": [
-              { "count": 1, "reps": 10, "weight": 30, "rest_time": 150 },
-              { "count": 2, "reps": 8, "weight": 35, "rest_time": 150 },
-              { "count": 3, "reps": 6, "weight": 40, "rest_time": 150 }
-            ]
+            "sets": []
           },
           {
             "name": "Barbell Row",
             "muscle_group": "Back",
             "equipment": "Barbell",
-            "sets": [
-              { "count": 1, "reps": 10, "weight": 50, "rest_time": 150 },
-              { "count": 2, "reps": 8, "weight": 60, "rest_time": 150 },
-              { "count": 3, "reps": 6, "weight": 70, "rest_time": 150 }
-            ]
+            "sets": []
           },
           {
             "name": "Dumbbell Curl",
             "muscle_group": "Biceps",
             "equipment": "Dumbbells",
-            "sets": [
-              { "count": 1, "reps": 12, "weight": 10, "rest_time": 60 },
-              { "count": 2, "reps": 10, "weight": 12, "rest_time": 60 },
-              { "count": 3, "reps": 8, "weight": 14, "rest_time": 60 }
-            ]
+            "sets": []
           },
           {
             "name": "Triceps Dips",
             "muscle_group": "Triceps",
             "equipment": "Dip Bars or Bench",
-            "sets": [
-              { "count": 1, "reps": 12, "weight": 0, "rest_time": 90 },
-              { "count": 2, "reps": 10, "weight": 0, "rest_time": 90 },
-              { "count": 3, "reps": 8, "weight": 0, "rest_time": 90 }
-            ]
+            "sets": []
           },
           {
             "name": "Plank",
             "muscle_group": "Core",
             "equipment": "Bodyweight",
-            "sets": [
-              { "count": 1, "reps": 1, "weight": 0, "rest_time": 60 },
-              { "count": 2, "reps": 1, "weight": 0, "rest_time": 60 },
-              { "count": 3, "reps": 1, "weight": 0, "rest_time": 60 }
-            ]
+            "sets": []
           },
           {
             "name": "Lunges",
             "muscle_group": "Legs, Glutes",
             "equipment": "Dumbbells (optional)",
-            "sets": [
-              { "count": 1, "reps": 12, "weight": 0, "rest_time": 90 },
-              { "count": 2, "reps": 12, "weight": 0, "rest_time": 90 },
-              { "count": 3, "reps": 12, "weight": 0, "rest_time": 90 }
-            ]
+            "sets": []
           },
         ]);
       }
